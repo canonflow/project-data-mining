046ed12b06ec030b24bfc4f70b0b83a28f1fc87c
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2023-12-19 00:06:22 +0700

    [Project] Fixing Bug Hosting

--- kmeans-backend.php
+++ kmeans-backend.php
@@ -210,9 +210,7 @@
         }
         return false;
     }
-?>
 
-<?php 
     if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
         header('HTTP/1.1 405 Method Not Allowed');
         echo 'Method Not Allowed';
